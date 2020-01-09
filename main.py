584a03118ddd65b5d46076fdc98876ef221f9d28
The Most Boring Person In The World Patryk Zmyslony <selppkez@gmail.com>
2020-01-09 13:44:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@
 
 
 users = {
-'0':["FirstUser", "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", "Patryk Zmyslony", True, 0],
-'1':["SecondUser", "60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752", "John Wick", False, 0]
+0:["FirstUser", "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", "Patryk Zmyslony", True, 0],
+1:["SecondUser", "60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752", "John Wick", False, 0]
 }
 unsuccessfull = 3
 #if users["0"][1] == password:
-
+welcomessage = "Hello Agent "
 
 f=open("logins.txt", "r")
 #e=open("logins.txt", "w")
@@ -23,3 +23,9 @@ newdict = eval(contente)
 #contente.users["0"][1]
 
 #print(newdict["0"][1])
+
+agentID = input("Please enter your agent ID:\n")
+
+for k in newdict:
+	if agentID == k in newdict:
+		print(welcomessage+newdict[agentID][2])
